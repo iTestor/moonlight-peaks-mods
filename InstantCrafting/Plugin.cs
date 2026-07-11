9c96c26dde47b957fed2d067341e4d67b0aadd4b
iTestor <91094276+iTestor@users.noreply.github.com>
2026-07-11 21:17:23 +0200

    Add all InstantCrafting files

---
+++ InstantCrafting/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace InstantCrafting
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource _logger;
+        internal static Harmony _harmony;
+
+        public static ConfigEntry<bool> InstantEnabled;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            _logger = base.Logger;
+            _logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+            InstantEnabled = Config.Bind("General", "InstantEnabled", true, "Enable or disable instant crafting.");
+
+            _harmony = new Harmony("dev.kuri.moonlightpeaks.instantcrafting");
+            _harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(MicrogameDifficultyAsset), "Difficulty", MethodType.Getter)]
+    public static class MicrogameDifficultyAsset_get_Difficulty_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref int __result)
+        {
+            __result = 0;
+        }
+    }
+}
